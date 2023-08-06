f75e8abba91162a7eaa13db9a455faf1ca00ae14
Max Gatchang <maxgatchang96@gmail.com>
2023-08-07 01:38:16 +0300

    Number three of the exercise

---
+++ src/main/kotlin/firstNumber.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //This is the first number
+    //create an array of 50 integers and use a for loop to print out the names of the integer
+
+    val integers = Array(50) { it + 1 }
+    for (number in integers) {
+        println(number)
+    }
+}
